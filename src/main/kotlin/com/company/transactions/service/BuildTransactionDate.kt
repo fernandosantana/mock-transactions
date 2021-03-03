bbfbadbd60fd2172532e298d5d01676e6b16c107
Luis Fernando <l.fernandosantana@gmail.com>
2021-03-02 22:49:02 -0300

    refactor: change to retorn epochSecond

--- src/main/kotlin/com/company/transactions/service/BuildTransactionDate.kt
+++ src/main/kotlin/com/company/transactions/service/BuildTransactionDate.kt
@@ -2,12 +2,14 @@ package com.company.transactions.service
 
 import org.springframework.stereotype.Service
 import java.time.LocalDate
+import java.time.ZoneId
 
 @Service
 class BuildTransactionDate {
 
     fun getDate(month: Int, year: Int): Long {
         val day = (1..28).random()
-        return LocalDate.of(year, month, day).toEpochDay()
+        val date = LocalDate.of(year, month, day)
+        return date.atStartOfDay(ZoneId.systemDefault()).toInstant().epochSecond
     }
 }
