45f047078c660acae1a08db502132f81a8044b25
Luis Fernando <l.fernandosantana@gmail.com>
2021-03-04 21:38:42 -0300

    test: add others codes when min size

--- src/test/kotlin/com/company/transactions/service/BuildCodeSyllablesToTransactionTest.kt
+++ src/test/kotlin/com/company/transactions/service/BuildCodeSyllablesToTransactionTest.kt
@@ -1,4 +1,31 @@
 package com.company.transactions.service
 
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
 class BuildCodeSyllablesToTransactionTest {
+
+    @Test
+    fun `must return sillable list whithout zeros`() {
+        val id = 100000
+        val year = 2021
+        val month = 10
+
+        val buildCodeSyllabes = BuildCodeSyllablesToTransaction()
+        val codeSyllablesList = buildCodeSyllabes.getSyllables(id, year, month)
+
+        assertFalse(codeSyllablesList.contains(0))
+    }
+
+    @Test
+    fun `must return code sillable list with space code - mean two words`() {
+        val id = 100000
+        val year = 2021
+        val month = 10
+
+        val buildCodeSyllabes = BuildCodeSyllablesToTransaction()
+        val codeSyllablesList = buildCodeSyllabes.getSyllables(id, year, month)
+
+        assertTrue(codeSyllablesList.contains(32))
+    }
 }
